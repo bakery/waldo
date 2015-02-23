d0658e5ce281a66e78fed1fb6cedb914a4b1f114
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2015-02-23 22:01:57 +0800

    rebuild post ui

--- client/views/items/post.js
+++ client/views/items/post.js
@@ -1,4 +1,10 @@
 Template.postItem.events({
+
+	'click .post' : function(){
+		var data = Template.instance().data;
+		Router.go('showPost', { _id : data.post._id });
+	},
+
     'click .post-content > a' : function(e){
         e.stopImmediatePropagation();
         window.open($(e.target).attr('href'), '_blank', 'location=yes');
