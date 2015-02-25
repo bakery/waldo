14a84af8f8c588c20b0bf195f705be89c83db153
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2015-02-25 20:16:04 +0800

    implement infinite scroll on the main feed

--- client/views/pages/feed.js
+++ client/views/pages/feed.js
@@ -1,10 +1,25 @@
 var subs = new SubsManager();
+var postsPerPage = Meteor.settings.public.waldo.feed.postsPerPage;
 
 Template.feed.created = function () {
+    
+    Template.feed.current = this;
+
     var instance = this;
     //instance.loaded = new ReactiveVar(0);
     
+    instance.limit = new ReactiveVar(postsPerPage);
+    instance.hasMore = new ReactiveVar(true);
     instance.data.ready = new ReactiveVar(false);
+
+    instance.data.posts = function() { 
+        return Posts.find({}, {
+            sort : {
+                createdAt : -1
+            },
+            limit: Template.feed.current.limit.get()
+        });
+    };
 
     instance.autorun(function () {
         var location = Helpers.getCurrentLocationOnce();
@@ -13,24 +28,32 @@ Template.feed.created = function () {
             var subscription = subs.subscribe('feed', {
                 latitude : location.lat, 
                 longitude : location.lng
-            });
+            }, instance.limit.get());
 
             // if subscription is ready, set limit to newLimit
             if (subscription.ready()) {
+                instance.hasMore.set(
+                    Template.feed.current.data.posts().count() >= Template.feed.current.limit.get()
+                );
+
                 instance.data.ready.set(true);
             } else {
                 instance.data.ready.set(false);
             }
         }
     });
+};
 
-    instance.data.posts = function() { 
-        return Posts.find({}, {
-            sort : {
-                createdAt : -1
-            }
-        });
-    }
+Template.feed.rendered = function(){
+    this.$('.content').scroll(function(e){
+        if(Template.feed.current.hasMore.get()){
+            ScrollHelper.checkInfiniteBounds($('.content')[0], function(){
+                if(Template.feed.current.data.ready.get()){
+                    Template.feed.current.limit.set(Template.feed.current.limit.get()+postsPerPage);
+                }
+            });
+        }
+    });
 };
 
 Template.feed.helpers({
