6cf72f7f4e16f0a5b1541767b25d572dfbaec01b
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2015-02-26 17:57:58 +0800

    reorganize feed view so it does not break on mobile

--- client/views/pages/feed.js
+++ client/views/pages/feed.js
@@ -1,18 +1,15 @@
 var subs = new SubsManager();
-var postsPerPage = Meteor.settings.public.waldo.feed.postsPerPage;
 
 Template.feed.created = function () {
-    
-    Template.feed.current = this;
 
-    var instance = this;
-    //instance.loaded = new ReactiveVar(0);
+    var postsPerPage = Meteor.settings.public.waldo.feed.postsPerPage;
     
-    instance.limit = new ReactiveVar(postsPerPage);
-    instance.hasMore = new ReactiveVar(true);
-    instance.data.ready = new ReactiveVar(false);
+    Template.feed.current = {};
+    Template.feed.current.limit = new ReactiveVar(postsPerPage);
+    Template.feed.current.hasMore = new ReactiveVar(true);
+    Template.feed.current.ready = new ReactiveVar(false);
 
-    instance.data.posts = function() { 
+    Template.feed.current.posts = function() { 
         return Posts.find({}, {
             sort : {
                 createdAt : -1
@@ -21,34 +18,37 @@ Template.feed.created = function () {
         });
     };
 
-    instance.autorun(function () {
+    this.autorun(function () {
         var location = Helpers.getCurrentLocationOnce();
 
         if(location){
             var subscription = subs.subscribe('feed', {
                 latitude : location.lat, 
                 longitude : location.lng
-            }, instance.limit.get());
+            }, Template.feed.current.limit.get());
 
             // if subscription is ready, set limit to newLimit
             if (subscription.ready()) {
-                instance.hasMore.set(
-                    Template.feed.current.data.posts().count() >= Template.feed.current.limit.get()
+                Template.feed.current.hasMore.set(
+                    Template.feed.current.posts().count() >= Template.feed.current.limit.get()
                 );
 
-                instance.data.ready.set(true);
+                Template.feed.current.ready.set(true);
             } else {
-                instance.data.ready.set(false);
+                Template.feed.current.ready.set(false);
             }
         }
     });
 };
 
 Template.feed.rendered = function(){
+
+    var postsPerPage = Meteor.settings.public.waldo.feed.postsPerPage;
+
     this.$('.content').scroll(function(e){
         if(Template.feed.current.hasMore.get()){
             ScrollHelper.checkInfiniteBounds($('.content')[0], function(){
-                if(Template.feed.current.data.ready.get()){
+                if(Template.feed.current.ready.get()){
                     Template.feed.current.limit.set(Template.feed.current.limit.get()+postsPerPage);
                 }
             });
@@ -58,9 +58,9 @@ Template.feed.rendered = function(){
 
 Template.feed.helpers({
     posts: function () {
-        return Template.instance().data.posts();
+        return Template.feed.current.posts();
     },
     isReady: function () {
-        return Template.instance().data.ready.get();
+        return Template.feed.current.ready.get();
     }
 });
